b337d94a498b571856ccd1df68da5e18c866151b
Luiz Henrique <pereiraluiz5060@gmail.com>
2026-04-29 20:32:42 -0300

    Em desenvolvimento

--- js/script.js
+++ js/script.js
@@ -6,36 +6,32 @@ async function carregarProdutos() {
 
       dados.products.forEach((produto ) => {
         const card = `
-               <div class="col mb-5">
-            <div class="card h-100">
-              <!-- Product image-->
-              <img
-                class="card-img-top"
-                src="${produto.thumbnail}"
-                alt="${produto.title}"
-              />
-              <!-- Product details-->
-              <div class="card-body p-4">
+    <div class="col mb-5">
+        <div class="card h-100" style="background-color: #FAF9F6; border: none; box-shadow: 0 4px 6px rgba(0,0,0,0.1);">
+            <img class="card-img-top" src="${produto.thumbnail}" alt="${produto.title}" />
+            
+            <div class="card-body p-4">
                 <div class="text-center">
-                  <!-- Product name-->
-                  <h5 class="fw-bolder">${produto.title}</h5>
-                  <!-- Product price-->
-                  R$${produto.price}
+                    <h5 class="fw-bolder" style="color: #333333">${produto.title}</h5>
+                    <span style="color: #B4C79F; font-weight: bold; font-size: 1.2rem;">
+                        R$ ${produto.price}
+                    </span>
                 </div>
-              </div>
-              <!-- Product actions-->
-              <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center">
-                  <a class="btn btn-outline-dark mt-auto" href="#"
-                    >Ver mais</a
-                  >
-                </div>
-              </div>
             </div>
-          </div>
-        `;
-        container.innerHTML += card;
-      });
+            
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center">
+                    <a class="btn mt-auto" href="#" 
+                       style="background-color: #B4C79F; color: white; border-radius: 25px; padding: 8px 20px;">
+                       Ver detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    container.innerHTML += card;
+});
      }
 
     catch (error) {
